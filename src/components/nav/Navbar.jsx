ef8982ff1c04fb7da1dda3c15e49bc3963bc0a8e
Prosper Success <osiletusuccess@gmail.com>
2022-09-15 22:23:29 +0100

    corrected mispelt folder

---
+++ src/components/nav/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { HiOutlineHome } from "react-icons/hi";
+import { HiVideoCamera } from "react-icons/hi";
+import { HiOutlinePhone } from "react-icons/hi";
+import { HiUsers } from "react-icons/hi";
+import { SiAboutdotme } from "react-icons/si";
+
+const Navbar = () => {
+  return (
+    <div className="navbar__container flex rounded-xl shadow-md shadow-cyan-300">
+      <ul className="justify-center text-center">
+        <li>
+          <a href="">
+            {" "}
+            <HiOutlineHome className=" text-2xl text-cyan-300" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <HiVideoCamera className="text-2xl text-cyan-300" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <HiOutlinePhone className="text-2xl text-cyan-300" />
+          </a>
+        </li>
+        <li>
+          <a href="#about">
+            <SiAboutdotme href="" className="text-2xl text-cyan-300" />
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <HiUsers className="text-2xl text-cyan-300" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;

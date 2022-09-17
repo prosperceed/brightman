85092175a4525393fca3a7f04b3ef0472abf77ce
Prosper Success <osiletusuccess@gmail.com>
2022-09-17 23:53:47 +0100

    adjusted text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
         <div className="header__img bg-cyan-400 mx-10 w-[20rem] h-[27rem] shadow-lg mb-8">
           <img className=" h-[23rem] mt-10 object-cover" src={ME} alt="me" />
         </div>
-        <div className="header__text text-center tracking-wide leading-[2rem]">
-          <h3 className="text-3xl text-white font-bold">
+        <div className="header__text text-center tracking-wide leading-[2rem] mx-6">
+          <h3 className="text-3xl intro__text text-white font-bold">
             Welcome To My{" "}
             <small className=" uppercase">
               Domain. I look forward to working with you😂

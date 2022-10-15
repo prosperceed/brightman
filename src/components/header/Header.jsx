1e135bab0d6bf4eaab4b124118d0d83210326c99
Prosper Success <osiletusuccess@gmail.com>
2022-10-15 13:36:16 +0100

    centered image on small device

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <div className="header__container">
       <div className="header__content pt-20 grid justify-between md:justify-center md:grid-cols-2">
-        <div className="header__img left-30 md:left-0 bg-cyan-400 mx-10 w-[20rem] h-[23rem] shadow-lg mb-8">
+        <div className="header__img right-4 md:left-0 bg-cyan-400 mx-10 w-[20rem] h-[23rem] shadow-lg mb-8">
           <img
             className=" h-[22rem] rounded-full mx-auto w-[180px] object-cover"
             src={ME}
             alt="me"
           />
         </div>
-        <div className="header__text mx-8 md:justify-center">
+        <div className="header__text mt-6 mx-8 md:justify-center">
           <h3 className="text-2xl text-center mr-10 md:mr-0 md:text-justify tracking-[0.2rem] intro__text text-slate-300 font-bold">
             Welcome to{" "}
             <small className="text-cyan-300 text-2xl">
